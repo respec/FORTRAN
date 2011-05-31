c862ae3163dc9ca5364998ba5592452cef4e1478
prhummel <prhummel@e0c5ff61-2711-0410-ab50-a8ab45870fe7>
2011-05-31 21:17:12 +0000

    emadata.for, j407xe.for - revised call to EMADATA to include Interval data StationData.f90 - added XQUAL (quality code) and IPSEQ (peak year) elements to be returned to PKFQWin

--- f90apps/peakfq.new/src/StationData.f90
+++ f90apps/peakfq.new/src/StationData.f90
@@ -4,9 +4,11 @@
 
       TYPE StnDat
         CHARACTER (LEN=80) :: HEADER
+        CHARACTER (LEN=5)  :: XQUAL(200)
         INTEGER            :: NPLOT
         INTEGER            :: NPKPLT
         INTEGER            :: HSTFLG
+        INTEGER            :: IPKSEQ(200)
         REAL               :: WEIBA
         REAL               :: PKLOG(200)
         REAL               :: SYSPP(200)
