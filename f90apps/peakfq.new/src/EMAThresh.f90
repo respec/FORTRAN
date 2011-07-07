005ea5dea3064032d1e3d17823d259cee7fb280a
Paul Hummel <PaulHummelRESPEC@users.noreply.github.com>
2011-07-07 14:00:31 +0000

    EMAThresh.f90 - added PeakSpec type and NEWPKS array; added comment variables for threshold and interval types j407xe.for - added UPDATEPEAKS routine to incorporate new/revised peak values;              added call to UPDATEPEAKS after INPUT; added IQUAL as arg to PRTIN2 and PRTIN3 in an attempt to  output NWIS code (looks like a FAIL);              added non-log version of PKS to arg list in STOREDATA for retrieval by windows interface; revised table formats in PRTINP, PRTIN2, and PRTIN3;              added GETPEAKS for retrieval of peaks by windows interface peakfq.for - revised PARSESTASPECS to process threshold/interval comments and to process PEAK specifications;              revised WRITESTASPECS to write PEAK specifications

--- f90apps/peakfq.new/src/EMAThresh.f90
+++ f90apps/peakfq.new/src/EMAThresh.f90
@@ -1,20 +1,30 @@
       MODULE EMAThresh
 
-      PUBLIC ThreshSpec, IntervalSpec
+      PUBLIC ThreshSpec, IntervalSpec, PeakSpec
 
       TYPE ThreshSpec
         INTEGER :: THRBYR, THREYR, NOBS
         REAL    :: THRLWR, THRUPR, THPP
+        CHARACTER*80 :: THRCOM
       END TYPE
 
       TYPE IntervalSpec
         INTEGER :: INTRVLYR
         REAL    :: INTRVLLWR, INTRVLUPR
+        CHARACTER*80 :: INTRVLCOM
+      END TYPE
+
+      TYPE PeakSpec
+        INTEGER :: PKYR
+        REAL    :: PKVAL
+        CHARACTER*5  :: PKCODE
+        CHARACTER*80 :: PKCOM
       END TYPE
 
       TYPE (ThreshSpec), ALLOCATABLE :: THRESH(:)
       TYPE (IntervalSpec), ALLOCATABLE :: INTERVAL(:)
-      INTEGER NTHRESH, THRDEF, NINTERVAL
+      TYPE (PeakSpec), ALLOCATABLE :: NEWPKS(:)
+      INTEGER NTHRESH, THRDEF, NINTERVAL, NNEWPKS
 
       END
 
