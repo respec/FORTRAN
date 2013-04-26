00ca70cbb58437034d0922562ef8e54543a87bf0
pbduda <PaulDudaRESPEC@users.noreply.github.com>
2013-04-26 21:13:17 +0000

    one more change from Tong

--- lib3.0/SRC/HSPF124/routing.f95
+++ lib3.0/SRC/HSPF124/routing.f95
@@ -311,7 +311,7 @@ subroutine routing_execute(routingModel, routingStep)
 
         ! --- route flow through the drainage network
         if ( Nobjects(LINK) > 0 ) then
-            stepCount = flowrout_execute(Nobjects(LINK), SortedLinks, routingModel, routingStep)
+            mstepCount = flowrout_execute(Nobjects(LINK), SortedLinks, routingModel, routingStep)
         end if
     end if
 
