5871b7a61bc1ab808528d0ab781ae362afc8def2
PaulDudaRESPEC <paul.duda@respec.com>
2017-03-13 16:27:36 -0400

    added more testing to vb driver

--- vbDrivers/WdmEntDriverVB/WdmEntDriver.vb
+++ vbDrivers/WdmEntDriverVB/WdmEntDriver.vb
@@ -19,11 +19,39 @@
     Public Sub F90_WDDSNX(ByRef aWdmsFile As Integer, ByRef aDsn As Integer)
     End Sub
     <DllImport("WdmEnt.dll", CallingConvention:=CallingConvention.Cdecl, CharSet:=CharSet.Ansi)>
+    Public Sub F90_WTFNDT(ByRef aWdmsFile As Integer, ByRef aDsn As Integer, ByRef aGpflg As Integer, ByRef aTdsfrc As Integer, ByVal aSDate() As Integer, ByVal aEDate() As Integer, ByRef aRetcod As Integer)
+    End Sub
+    <DllImport("WdmEnt.dll", CallingConvention:=CallingConvention.Cdecl, CharSet:=CharSet.Ansi)>
     Public Sub F90_WDTGET(ByRef aWdmsFile As Integer, ByRef aDsn As Integer, ByRef aDelt As Integer, ByVal aDates() As Integer,
                           ByRef aNval As Integer, ByRef aDtran As Integer, ByRef aQualfg As Integer, ByRef aTunits As Integer, ByVal aRval() As Single, ByRef aRetcod As Integer)
     End Sub
     <DllImport("WdmEnt.dll", CallingConvention:=CallingConvention.Cdecl, CharSet:=CharSet.Ansi)>
-    Public Sub F90_WTFNDT(ByRef aWdmsFile As Integer, ByRef aDsn As Integer, ByRef aGpflg As Integer, ByRef aTdsfrc As Integer, ByVal aSDate() As Integer, ByVal aEDate() As Integer, ByRef aRetcod As Integer)
+    Public Sub F90_WDTPUT(ByRef aWdmsFile As Integer, ByRef aDsn As Integer, ByRef aDelt As Integer, ByVal aDates() As Integer,
+                          ByRef aNval As Integer, ByRef aOvfg As Integer, ByRef aQualfg As Integer, ByRef aTunits As Integer, ByVal aRval() As Single, ByRef aRetcod As Integer)
+    End Sub
+    <DllImport("WdmEnt.dll", CallingConvention:=CallingConvention.Cdecl, CharSet:=CharSet.Ansi)>
+    Public Sub F90_WDBSGI(ByRef aWdmsFile As Integer, ByRef aDsn As Integer, ByRef aSaind As Integer, ByRef aSalen As Integer,
+                          ByVal aSaval As Integer, ByRef aRetcod As Integer)
+    End Sub
+    <DllImport("WdmEnt.dll", CallingConvention:=CallingConvention.Cdecl, CharSet:=CharSet.Ansi)>
+    Public Sub F90_WDBSAI(ByRef aWdmsFile As Integer, ByRef aDsn As Integer, ByRef aMsFile As Integer, ByRef aSaind As Integer, ByRef aSalen As Integer,
+                          ByRef aSaval As Integer, ByRef aRetcod As Integer)
+    End Sub
+    <DllImport("WdmEnt.dll", CallingConvention:=CallingConvention.Cdecl, CharSet:=CharSet.Ansi)>
+    Public Sub F90_WDBSGR(ByRef aWdmsFile As Integer, ByRef aDsn As Integer, ByRef aSaind As Integer, ByRef aSalen As Integer,
+                          ByVal aSaval As Single, ByRef aRetcod As Integer)
+    End Sub
+    <DllImport("WdmEnt.dll", CallingConvention:=CallingConvention.Cdecl, CharSet:=CharSet.Ansi)>
+    Public Sub F90_WDBSAR(ByRef aWdmsFile As Integer, ByRef aDsn As Integer, ByRef aMsFile As Integer, ByRef aSaind As Integer, ByRef aSalen As Integer,
+                          ByRef aSaval As Single, ByRef aRetcod As Integer)
+    End Sub
+    <DllImport("WdmEnt.dll", CallingConvention:=CallingConvention.Cdecl, CharSet:=CharSet.Ansi)>
+    Public Sub F90_WDBSGC(ByRef aWdmsFile As Integer, ByRef aDsn As Integer, ByRef aSaind As Integer, ByRef aSalen As Integer,
+                          ByVal aSaval() As Integer)
+    End Sub
+    <DllImport("WdmEnt.dll", CallingConvention:=CallingConvention.Cdecl, CharSet:=CharSet.Ansi)>
+    Public Sub F90_WDBSAC(ByRef aWdmsFile As Integer, ByRef aDsn As Integer, ByRef aMsFile As Integer, ByRef aSaind As Integer, ByRef aSalen As Integer,
+                          ByRef aRetcod As Integer, ByRef aSaval As String, ByVal aStringLength As Short)
     End Sub
 
     Dim gOutFileStream As IO.FileStream
@@ -31,8 +59,8 @@
     Sub Main()
 
         'local(variables)
-        Dim WDMSFL, RETCOD, DSN, DSTYPE, I As Integer
-        Dim WDNAME, WDMNAMES(3) As String
+        Dim WDMSFL, MESSFL, RETCOD, DSN, DSTYPE, I As Integer
+        Dim WDNAME, WDMNAMES(3), MSNAME As String
         Dim lOutStream() As Byte
 
         Debug.Print("Start WdmEntDriverFortran")
@@ -93,6 +121,47 @@
                     lOutStream = (System.Text.Encoding.Unicode.GetBytes("  F90_WDTGET: DSN, RVAL: " & DSN & ", " & lRval(0).ToString & vbCrLf))
                     gOutFileStream.Write(lOutStream, 0, lOutStream.Length)
 
+                    Dim lOvfg As Integer = 1
+                    lRval(1) = 1.0
+                    lRval(2) = 2.0
+                    lRval(3) = 3.0
+                    F90_WDTPUT(WDMSFL, DSN, lDelt, lDates, lNvals, lOvfg, lQual, lTunit, lRval, RETCOD)
+                    lOutStream = (System.Text.Encoding.Unicode.GetBytes("  F90_WDTPUT: DSN, RETCOD: " & DSN & ", " & RETCOD))
+
+                    'work with attributes
+                    'have to open message wdm first
+                    MSNAME = "\FORTRAN\lib3.0\hspfmsg.wdm"
+                    MESSFL = F90_WDBOPN(1, MSNAME, Len(MSNAME))
+
+                    Dim lSaind As Integer = 34
+                    Dim lSalen As Integer = 1
+                    Dim lSaval As Integer
+                    F90_WDBSGI(WDMSFL, DSN, lSaind, lSalen, lSaval, RETCOD)
+                    lOutStream = (System.Text.Encoding.Unicode.GetBytes("  F90_WDBSGI: SAVAL, RETCOD: " & lSaval & ", " & RETCOD))
+
+                    F90_WDBSAI(WDMSFL, DSN, MESSFL, lSaind, lSalen, lSaval, RETCOD)
+                    lOutStream = (System.Text.Encoding.Unicode.GetBytes("  F90_WDBSAI: SAVAL, RETCOD: " & lSaval & ", " & RETCOD))
+
+                    lSaind = 7
+                    lSalen = 1
+                    Dim lRsaval As Integer
+                    F90_WDBSGR(WDMSFL, DSN, lSaind, lSalen, lRsaval, RETCOD)
+                    lOutStream = (System.Text.Encoding.Unicode.GetBytes("  F90_WDBSGR: SAVAL, RETCOD: " & lRsaval & ", " & RETCOD))
+
+                    F90_WDBSAR(WDMSFL, DSN, MESSFL, lSaind, lSalen, lRsaval, RETCOD)
+                    lOutStream = (System.Text.Encoding.Unicode.GetBytes("  F90_WDBSAR: SAVAL, RETCOD: " & lRsaval & ", " & RETCOD))
+
+                    lSaind = 45
+                    lSalen = 48
+                    Dim lIval() As Integer
+                    F90_WDBSGC(WDMSFL, DSN, lSaind, lSalen, lIval)
+                    'need to turn these integer values back into a character string
+                    lOutStream = (System.Text.Encoding.Unicode.GetBytes("  F90_WDBSGC: IVAL, RETCOD: " & lIval(1) & ", " & RETCOD))
+
+                    Dim lCval As String = "Test Stanam"
+                    F90_WDBSAC(WDMSFL, DSN, MESSFL, lSaind, lSalen, RETCOD, lCval, Len(lCval))
+                    lOutStream = (System.Text.Encoding.Unicode.GetBytes("  F90_WDBSAC: CVAL, RETCOD: " & lCval & ", " & RETCOD))
+
                     RETCOD = F90_WDFLCL(WDMSFL)
                     lOutStream = (System.Text.Encoding.Unicode.GetBytes("  F90_WDFLCL Return Code " & RETCOD & vbCrLf))
                     gOutFileStream.Write(lOutStream, 0, lOutStream.Length)
@@ -111,7 +180,7 @@
     Function Find_Test_Folder() As Boolean
         Try
             Dim Cdir As String = CurDir()
-            Debug.Print("Base directory is: " & Cdir)
+            Debug.Print("Base directory Is:       " & Cdir)
             Dim BaseFolderParts() As String = Cdir.Split("\")
             Dim l As Integer = UBound(BaseFolderParts)
             Dim ResultsFileName As String = BaseFolderParts(l - 1) & "!" & BaseFolderParts(l) & "!Results.out"
